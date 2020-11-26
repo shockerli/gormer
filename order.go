fb6b763f427b56bf2b3ef7b38f9f1a0cc8c1cb74
shockerli <shockerli@upfor.club>
2020-11-26 10:24:16 +0800

    mod: order receiver

--- order.go
+++ order.go
@@ -6,18 +6,22 @@ import (
 	"github.com/jinzhu/gorm"
 )
 
-// 统一的排序参数
+// OrderParam
 type OrderParam struct {
 	OrderBy   string `json:"order_by"` // 可以是具体字段名，也可以是逗号分隔的多个排序
 	OrderType string `json:"order_type"`
 }
 
-// 处理排序
-func ScopeOrderBy(param *OrderParam) func(db *gorm.DB) *gorm.DB {
+// OrderBy scope: order by
+func (p *OrderParam) Order() func(db *gorm.DB) *gorm.DB {
 	return func(db *gorm.DB) *gorm.DB {
-		if param != nil && param.OrderBy != "" {
-			order := param.OrderBy
-			if !strings.Contains(param.OrderBy, ",") && strings.ToLower(param.OrderType) == "desc" {
+		if p != nil && p.OrderBy != "" {
+			order := p.OrderBy
+			if strings.ToUpper(p.OrderType) == "DESC" &&
+				!strings.Contains(p.OrderBy, ",") &&
+				!strings.Contains(strings.ToUpper(p.OrderBy), " ASC") &&
+				!strings.Contains(strings.ToUpper(p.OrderBy), " DESC") {
+
 				order += " DESC"
 			}
 			return db.Order(order)
