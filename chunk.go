3a4ecf8eeb2d9facbb7c0a1710e036103d24a061
shockerli <shockerli@upfor.club>
2021-07-23 15:17:47 +0800

    fix chunk where scope reuse

--- chunk.go
+++ chunk.go
@@ -1,74 +1,78 @@
 package gormer
 
 import (
+	"database/sql"
 	"fmt"
+	"time"
 
 	"github.com/jinzhu/gorm"
 )
 
 // ChunkByIDMaxMin process data in chunks, scope by id
-func ChunkByIDMaxMin(size int, db *gorm.DB, dest interface{}, callback func(), l Logger) {
+func ChunkByIDMaxMin(size int64, db *gorm.DB, dest interface{}, callback func(), l Logger) {
 	if l == nil {
 		l = new(DefaultLogger)
 	}
+	startTime := time.Now().UnixNano()
 
 	// query the maximum and minimum primary key id that satisfy the criteria
 	var stat = struct {
-		MaxID int `json:"max_id"`
-		MinID int `json:"min_id"`
+		MaxID sql.NullInt64 `json:"max_id"`
+		MinID sql.NullInt64 `json:"min_id"`
 	}{}
-	err := db.Debug().
-		Select("MAX(id) AS max_id, MIN(id) AS min_id").
-		Take(&stat).Error
+	err := db.NewScope(db).DB().Select("MAX(id) AS max_id, MIN(id) AS min_id").Take(&stat).Error
 
-	if (err != nil && gorm.IsRecordNotFoundError(err)) || stat.MaxID == 0 {
-		l.Info("no matching data...")
-		return
-	}
 	if err != nil {
-		l.Error(fmt.Sprintf("query MaxId and MinId error: %v", err))
+		l.Error(fmt.Sprintf("query MinId and MaxId error: %v", err))
+		return
+	}
+	if !stat.MaxID.Valid || !stat.MinID.Valid {
+		l.Info("no matching data...MinId(null), MaxId(null)")
 		return
 	}
 
-	l.Info(fmt.Sprintf("query result: MinId(%d), MaxId(%d)", stat.MinID, stat.MaxID))
+	l.Info(fmt.Sprintf("query result: MinId(%d), MaxId(%d)", stat.MinID.Int64, stat.MaxID.Int64))
 
 	// store the max id of last loop
-	var lastMaxID = stat.MinID
+	var lastMaxID = stat.MinID.Int64
 	var loop = 0
+	var totalCount int64
 
 	for {
 		loop++
 
-		if lastMaxID > stat.MaxID {
-			l.Info("data processing is completed...")
+		if lastMaxID > stat.MaxID.Int64 {
 			break
 		}
 
 		// start at MinId, end at MaxId
 		lt := lastMaxID + size
-		if lt > stat.MaxID {
-			lt = stat.MaxID + 1
+		if lt > stat.MaxID.Int64 {
+			lt = stat.MaxID.Int64 + 1
 		}
 
 		// paging through id range coverage
-		res := db.Where("id >= ?", lastMaxID).
-			Where("id < ?", lt).
+		res := db.NewScope(db).DB().
+			Where("? <= id AND id < ?", lastMaxID, lt).
 			Scan(dest)
 
-		l.Info(fmt.Sprintf("loop: %d, query result %d <= id < %d, count: %d, err: %v", loop, lastMaxID, lt, res.RowsAffected, res.Error))
+		l.Info(fmt.Sprintf("No.%d, query result %d <= id < %d, count: %d, err: %v", loop, lastMaxID, lt, res.RowsAffected, res.Error))
 
 		lastMaxID += size
+		totalCount += res.RowsAffected
 
 		// no data queried, continue next cycle
 		// if the id is discontinuous, it may detect that the data is empty,
-		//          but it does not mean that the loop is closed
+		// but it does not mean that the loop is closed
 		if res.Error != nil || res.RowsAffected <= 0 {
 			continue
 		}
 
 		// custom process by callback
 		callback()
-
 	}
+
+	usedTime := fmt.Sprintf("%.2fms", float64(time.Now().UnixNano()-startTime)/1e6)
+	l.Info(fmt.Sprintf("data processing is completed...Used: %s, TotalCount: %d", usedTime, totalCount))
 
 }
